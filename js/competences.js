fc78d4cbf26403d49cedf35e99580ba1cb1a18ea
JulesHeller <julesheller0@gmail.com>
2021-06-21 19:22:08 +0200

    oui

---
+++ js/competences.js
@@ -0,0 +1,32 @@
+/* DARK MODE */
+
+document.querySelector("header img").addEventListener("click", darkMode);
+
+function darkMode(){
+    document.querySelector("header").classList.toggle("fondNoir")
+
+    document.querySelectorAll("header a").forEach(e=>{
+        e.classList.toggle("aNoir")
+    })
+
+    document.getElementById("laPage").classList.toggle("laPage")
+
+    document.querySelector("main").classList.toggle("fondNoir")
+
+    document.querySelector("header img").classList.toggle("soleil")
+
+    document.querySelectorAll(".plein").forEach(e=>{
+        e.classList.toggle("darkPlein")
+    })
+    
+    document.querySelectorAll("h4").forEach(e=>{
+        e.classList.toggle("darkh4")
+    })
+
+    document.querySelector(".programmation").classList.toggle("whiteOutline")
+    document.querySelector(".suiteAdobe").classList.toggle("whiteOutline")
+
+    document.querySelectorAll("main img").forEach(e=>{
+        e.classList.toggle("logoBlanc")
+    })
+}
